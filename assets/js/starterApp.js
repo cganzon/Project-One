d3207cbb4a6ea474677fa876e3662bbecf15e9f6
cganzon <crganzon@gmail.com>
2019-03-11 23:42:30 -0700

    got lfw search function to work

--- assets/js/starterApp.js
+++ assets/js/starterApp.js
@@ -10,41 +10,10 @@ var config = {
 firebase.initializeApp(config);
 // =================================  
 
-
-function joobleSearch() {
-  var url = "https://us.jooble.org/api/";
-  var key = "63e7fe87-63ba-4b2b-9b1a-35a3e3d71f97";
-  var params = "{ keywords: 'Web Developer', location: 'San Diego'}"
-  // get parameters from user's input
-
-  //create xmlHttpRequest object
-  var http = new XMLHttpRequest();
-  //open connection. true - asynchronous, false - synchronous
-  http.open("POST", url + key, true);
-
-  //Send the proper header information
-  http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-  //Callback when the state changes
-  http.onreadystatechange = function () {
-    if (http.readyState == 4 && http.status == 200) {
-      var jsonResponse = JSON.parse(http.responseText);
-      console.log(jsonResponse);
-    }
-  }
-  //Send request to the server
-  http.send(params);
-}
-
-joobleSearch();
-
 // firebase connection
 var database = firebase.database();
 
-
-
-
-
+// When user signs up for an account
 $("#signUp").on("click", function (event) {
   //   // prevent default behaviour
   event.preventDefault();
@@ -68,10 +37,49 @@ $("#signUp").on("click", function (event) {
     resume: resumeLink,
     description: skillDescription,
   });
-
-
-
 });
+
+// get parameters from user's input for LFW
+$("#lfw-button").on("click", function (event) {
+  // prevents page refresh
+  event.preventDefault();
+  var titleSearch = $("#jobTitleSearchParam").val().trim();
+  var locationSearch = $("#locationSearchParam").val().trim();
+  var salarySearch = $("#salarySearchParam").val().trim();
+  console.log(titleSearch);
+  console.log(locationSearch);
+  console.log(salarySearch);
+
+  var results = joobleSearch(titleSearch, locationSearch, salarySearch);
+  console.log(results);
+})
+
+function joobleSearch(title, location, salary) {
+  var url = "https://us.jooble.org/api/";
+  var key = "63e7fe87-63ba-4b2b-9b1a-35a3e3d71f97";
+  // var params = "{ keywords: 'Web Developer', location: 'San Diego', salary: '60000'}";
+  var params = "{keywords: '" + title + "', location: '" + location + "', salary: '" + salary + "'}";
+
+  //create xmlHttpRequest object
+  var http = new XMLHttpRequest();
+  //open connection. true - asynchronous, false - synchronous
+  http.open("POST", url + key, true);
+
+  //Send the proper header information
+  http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+  //Callback when the state changes
+  http.onreadystatechange = function () {
+    if (http.readyState == 4 && http.status == 200) {
+      var jsonResponse = JSON.parse(http.responseText);
+      // console.log(jsonResponse);
+      return jsonResponse;
+    }
+  }
+  //Send request to the server
+  http.send(params);
+}
+
 
 
 
